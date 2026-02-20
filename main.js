3033de2495e3d0e0ec12db9817ff60027997fadc
visorde-9e-ay2526 <visorde.cm@obmontessori.edu.ph>
2026-02-20 11:28:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function buttonDiscount (){
+    let budget = document.getElementById("budget").value;
+    let discountedPrice = Number(origBudget * 0.2);
+
+    document.getElementById().innerHTML=
+}
